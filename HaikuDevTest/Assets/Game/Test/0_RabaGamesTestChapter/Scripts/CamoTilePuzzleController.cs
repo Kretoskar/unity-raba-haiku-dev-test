755edec6082cc0b281f59a8e5baeb323b6b01f48
Oskar <kogutoskar123@gmail.com>
2020-01-26 09:49:40 +0100

    Added puzzle navigation

---
+++ HaikuDevTest/Assets/Game/Test/0_RabaGamesTestChapter/Scripts/CamoTilePuzzleController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.MudRoom.Puzzle
+{
+    public class CamoTilePuzzleController : PuzzleController
+    {
+        private void Start()
+        {
+            Activate();
+            print("xd");
+        }
+    }
+}
